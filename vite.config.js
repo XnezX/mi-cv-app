50c66f4c46edd2b59840198d2c3bc469919f0d5f
DanyDev <danielma1507@gmail.com>
2024-10-14 11:12:09 -0600

    Tu mensaje de commit aquí

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mi-cv-app/', // Cambia 'mi-cv-app' por el nombre de tu repositorio
   plugins: [react()],
+  base: '/mi-cv-app/', // Asegúrate de que el nombre del repositorio sea correcto
+  build: {
+    rollupOptions: {
+      external: ['/mi-cv-app/assets/index-DoTWbw-e.js', '/mi-cv-app/assets/index-DbxInE2X.css'], // Puedes agregar otras dependencias externas aquí si es necesario
+    },
+  },
 });
+
 
 
